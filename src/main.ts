804556742bf74c339490e3c06a1dd073c0a58fb6
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-10 15:37:40 +0300

    Move swagger definition

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,7 @@
-import { INestApplication } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
-import { BotModule } from './bot/bot.module'
-
-function createSwagger(app: INestApplication) {
-    const moobotOptions = new DocumentBuilder()
-        .setTitle('Kicshikxo Twitch bot API')
-        .setDescription('<a href="https://github.com/Kicshikxo/Moobot-commands#readme" target="_blank"><b>Документация</b></a>')
-        .setVersion('')
-        .build()
-
-    const moobotDocument = SwaggerModule.createDocument(app, moobotOptions, {
-        include: [BotModule]
-    })
-
-    SwaggerModule.setup('/', app, moobotDocument, {
-        customCss: '.swagger-ui .topbar { display: none } * { outline: none !important }',
-        customSiteTitle: 'Kicshikxo Twitch bot API'
-    })
-}
+import { createSwagger } from './swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
---
+++ src/swagger.ts
@@ -0,0 +1,26 @@
+import { INestApplication } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { BotModule } from './module/bot.module'
+
+export const createDocument = (app: INestApplication) => {
+    const options = new DocumentBuilder()
+        .setTitle('Kicshikxo Twitch bot API')
+        .setDescription('<a href="https://github.com/Kicshikxo/Twitch-bot#readme" target="_blank"><b>Документация</b></a>')
+        .setVersion('')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, options, {
+        include: [BotModule]
+    })
+
+    return document
+}
+
+export const createSwagger = (app: INestApplication, options?: { path?: string }) => {
+    const document = createDocument(app)
+
+    SwaggerModule.setup(options?.path ?? '/', app, document, {
+        customCss: '.swagger-ui .topbar { display: none } * { outline: none !important }',
+        customSiteTitle: 'Kicshikxo Twitch bot API'
+    })
+}
