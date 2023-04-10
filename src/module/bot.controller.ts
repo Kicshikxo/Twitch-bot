0b12ce734aab7853eb942925fac3fdbe25586a5c
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-10 21:42:57 +0300

    Remove unneeded bot controller

--- src/module/bot.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common'
-import { BotService } from './bot.service'
-
-@Controller()
-export class BotController {
-    constructor(private readonly botService: BotService) {}
-}
