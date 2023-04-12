f13fff08397862e8166cd412941a2eec03ef4fb2
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-12 12:22:53 +0300

    Add token password validation

--- src/module/auth/auth.middleware.ts
+++ src/module/auth/auth.middleware.ts
@@ -1,5 +1,7 @@
 import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common'
+import { crc32 } from 'crc'
 import { Request, Response } from 'express'
+import { PrismaService } from '../prisma/prisma.service'
 import { AuthService } from './auth.service'
 import { TokenDataDto } from './dto/token.data.dto'
 
@@ -11,12 +13,20 @@ declare module 'express' {
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService, private readonly prismaService: PrismaService) {}
 
-    use(req: Request, res: Response, next: () => void) {
+    async use(req: Request, res: Response, next: () => void) {
         req.tokenData = this.authService.readToken(req.headers.authorization?.substring('Bearer '.length))
         if (!req.tokenData) {
             throw new UnauthorizedException()
+        }
+
+        const channel = await this.prismaService.channel.findUnique({ where: { id: req.tokenData.id } })
+        if (!channel) {
+            throw new UnauthorizedException('Канал не найден')
+        }
+        if (req.tokenData.password !== crc32(channel.password).toString(16)) {
+            throw new UnauthorizedException('Пароль был изменён')
         }
 
         next()
